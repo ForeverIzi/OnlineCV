732580bf739ec4dccadd9de1e651c0e54e4bcf4f
ForeverIzi <davisjmcontato@gmail.com>
2025-07-22 12:53:28 -0300

    feat/carrousel-stacks-animation

--- script/script.js
+++ script/script.js
@@ -10,3 +10,24 @@ function toggleCourse(element) {
         }
     });
 };
+
+/* CARROUSEL STACK FUNCTION */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const track = document.querySelector('.tech-stack-track');
+    const items = document.querySelectorAll('.tech-item');
+    
+    const cloneItems = Array.from(items).map(item => item.cloneNode(true));
+    cloneItems.forEach(item => track.appendChild(item));
+    
+    const itemCount = items.length;
+    track.style.animationDuration = `${itemCount * 3}s`;
+    
+    track.addEventListener('animationiteration', () => {
+        track.style.transition = 'none';
+        track.style.transform = 'translateX(0)';
+        requestAnimationFrame(() => {
+            track.style.transition = 'transform 0s linear';
+        });
+    });
+});
